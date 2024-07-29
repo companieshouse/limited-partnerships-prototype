c2a76045a3d1a4d6ebd9b1f92c4d9e6c67a00bb1
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-07-29 08:18:31 +0100

    Added in conditional data routing (for varying LP types)

--- app/routes.js
+++ app/routes.js
@@ -11,3 +11,13 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.use('/', require('./routes/v2-routes.js'))
 
 
+// Show session data and URLs in the terminal  
+router.use((req, res, next) => {  
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+    next()  
+  }) 

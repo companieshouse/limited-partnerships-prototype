d914fb4ea35790eda65d9ac8304547b0d0d561b0
James Francis <jfranciswebdesign@users.noreply.github.com>
2025-04-17 07:56:44 +0100

    Updating default LP number

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -6,7 +6,7 @@ module.exports = {
 'emailAddress': 'agent@authorised-agent.com',
 'govOneLoginEmail': 'agent@authorised-agent.com',
 'registeredEmail': 'lp@limited-partnership.com',
-'lpNumber' : 'LP123456',
+'lpNumber' : 'SL123456',
 'lpChooseName' : 'ACME',
 'nameEnding' : 'LIMITED PARTNERSHIP',
 'uploadCompanyNumber' : 'LP123456'
